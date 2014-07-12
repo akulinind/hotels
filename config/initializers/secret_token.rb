929a631f6ebc03ebdbc79313773e01d3c3a2ea30
Dmitriy Akulinin <akulinind@gmail.com>
2014-07-12 11:53:46 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hotels::Application.config.secret_key_base = '71372cff10ee6761cfe1ba4858531e372c1dbc65d8c0a9bfc420d2025fd105bde2cb8143f910cff5ad5cdb9ca2edb1d103e06f738bcbdf64c02b8d8a1ea24d5f'

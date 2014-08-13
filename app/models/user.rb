937bedae0acfaa10a5a850f57ee57e7bd3404802
Dmitriy Akulinin <akulinind@gmail.com>
2014-08-13 22:25:31 +0400

    Add hotels and admins ammount to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_create :create_remember_token
+  has_many :hotels
+  has_many :rates
   
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
